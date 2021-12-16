7e2487271f89265e2522bfc1e598dc6a7e318864
kibrom <kibromhft@gmail.com>
2021-12-16 11:33:38 +0300

    CleanCode

--- Building_your_Deep_Neural_Network_Step_by_Step.py
+++ Building_your_Deep_Neural_Network_Step_by_Step.py
@@ -1,70 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Building your Deep Neural Network: Step by Step
-# 
-# Welcome to your week 4 assignment (part 1 of 2)! Previously you trained a 2-layer Neural Network with a single hidden layer. This week, you will build a deep neural network with as many layers as you want!
-# 
-# - In this notebook, you'll implement all the functions required to build a deep neural network.
-# - For the next assignment, you'll use these functions to build a deep neural network for image classification.
-# 
-# **By the end of this assignment, you'll be able to:**
-# 
-# - Use non-linear units like ReLU to improve your model
-# - Build a deeper neural network (with more than 1 hidden layer)
-# - Implement an easy-to-use neural network class
-# 
-# **Notation**:
-# - Superscript $[l]$ denotes a quantity associated with the $l^{th}$ layer. 
-#     - Example: $a^{[L]}$ is the $L^{th}$ layer activation. $W^{[L]}$ and $b^{[L]}$ are the $L^{th}$ layer parameters.
-# - Superscript $(i)$ denotes a quantity associated with the $i^{th}$ example. 
-#     - Example: $x^{(i)}$ is the $i^{th}$ training example.
-# - Lowerscript $i$ denotes the $i^{th}$ entry of a vector.
-#     - Example: $a^{[l]}_i$ denotes the $i^{th}$ entry of the $l^{th}$ layer's activations).
-# 
-# Let's get started!
-
-# ## Table of Contents
-# - [1 - Packages](#1)
-# - [2 - Outline](#2)
-# - [3 - Initialization](#3)
-#     - [3.1 - 2-layer Neural Network](#3-1)
-#         - [Exercise 1 - initialize_parameters](#ex-1)
-#     - [3.2 - L-layer Neural Network](#3-2)
-#         - [Exercise 2 - initialize_parameters_deep](#ex-2)
-# - [4 - Forward Propagation Module](#4)
-#     - [4.1 - Linear Forward](#4-1)
-#         - [Exercise 3 - linear_forward](#ex-3)
-#     - [4.2 - Linear-Activation Forward](#4-2)
-#         - [Exercise 4 - linear_activation_forward](#ex-4)
-#     - [4.3 - L-Layer Model](#4-3)
-#         - [Exercise 5 - L_model_forward](#ex-5)
-# - [5 - Cost Function](#5)
-#     - [Exercise 6 - compute_cost](#ex-6)
-# - [6 - Backward Propagation Module](#6)
-#     - [6.1 - Linear Backward](#6-1)
-#         - [Exercise 7 - linear_backward](#ex-7)
-#     - [6.2 - Linear-Activation Backward](#6-2)
-#         - [Exercise 8 - linear_activation_backward](#ex-8)
-#     - [6.3 - L-Model Backward](#6-3)
-#         - [Exercise 9 - L_model_backward](#ex-9)
-#     - [6.4 - Update Parameters](#6-4)
-#         - [Exercise 10 - update_parameters](#ex-10)
-
-# <a name='1'></a>
-# ## 1 - Packages
-# 
-# First, import all the packages you'll need during this assignment. 
-# 
-# - [numpy](www.numpy.org) is the main package for scientific computing with Python.
-# - [matplotlib](http://matplotlib.org) is a library to plot graphs in Python.
-# - dnn_utils provides some necessary functions for this notebook.
-# - testCases provides some test cases to assess the correctness of your functions
-# - np.random.seed(1) is used to keep all the random function calls consistent. It helps grade your work. Please don't change the seed! 
-
-# In[2]:
-
-
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
@@ -81,60 +14,6 @@ get_ipython().run_line_magic('load_ext', 'autoreload')
 get_ipython().run_line_magic('autoreload', '2')
 
 np.random.seed(1)
-
-
-# <a name='2'></a>
-# ## 2 - Outline
-# 
-# To build your neural network, you'll be implementing several "helper functions." These helper functions will be used in the next assignment to build a two-layer neural network and an L-layer neural network. 
-# 
-# Each small helper function will have detailed instructions to walk you through the necessary steps. Here's an outline of the steps in this assignment:
-# 
-# - Initialize the parameters for a two-layer network and for an $L$-layer neural network
-# - Implement the forward propagation module (shown in purple in the figure below)
-#      - Complete the LINEAR part of a layer's forward propagation step (resulting in $Z^{[l]}$).
-#      - The ACTIVATION function is provided for you (relu/sigmoid)
-#      - Combine the previous two steps into a new [LINEAR->ACTIVATION] forward function.
-#      - Stack the [LINEAR->RELU] forward function L-1 time (for layers 1 through L-1) and add a [LINEAR->SIGMOID] at the end (for the final layer $L$). This gives you a new L_model_forward function.
-# - Compute the loss
-# - Implement the backward propagation module (denoted in red in the figure below)
-#     - Complete the LINEAR part of a layer's backward propagation step
-#     - The gradient of the ACTIVATE function is provided for you(relu_backward/sigmoid_backward) 
-#     - Combine the previous two steps into a new [LINEAR->ACTIVATION] backward function
-#     - Stack [LINEAR->RELU] backward L-1 times and add [LINEAR->SIGMOID] backward in a new L_model_backward function
-# - Finally, update the parameters
-# 
-# <img src="images/final outline.png" style="width:800px;height:500px;">
-# <caption><center><b>Figure 1</b></center></caption><br>
-# 
-# 
-# **Note**:
-# 
-# For every forward function, there is a corresponding backward function. This is why at every step of your forward module you will be storing some values in a cache. These cached values are useful for computing gradients. 
-# 
-# In the backpropagation module, you can then use the cache to calculate the gradients. Don't worry, this assignment will show you exactly how to carry out each of these steps! 
-
-# <a name='3'></a>
-# ## 3 - Initialization
-# 
-# You will write two helper functions to initialize the parameters for your model. The first function will be used to initialize parameters for a two layer model. The second one generalizes this initialization process to $L$ layers.
-# 
-# <a name='3-1'></a>
-# ### 3.1 - 2-layer Neural Network
-# 
-# <a name='ex-1'></a>
-# ### Exercise 1 - initialize_parameters
-# 
-# Create and initialize the parameters of the 2-layer neural network.
-# 
-# **Instructions**:
-# 
-# - The model's structure is: *LINEAR -> RELU -> LINEAR -> SIGMOID*. 
-# - Use this random initialization for the weight matrices: `np.random.randn(shape)*0.01` with the correct shape
-# - Use zero initialization for the biases: `np.zeros(shape)`
-
-# In[3]:
-
 
 # GRADED FUNCTION: initialize_parameters
 
@@ -182,112 +61,6 @@ print("W2 = " + str(parameters["W2"]))
 print("b2 = " + str(parameters["b2"]))
 
 initialize_parameters_test(initialize_parameters)
-
-
-# ***Expected output***
-# ```
-# W1 = [[ 0.01624345 -0.00611756 -0.00528172]
-#  [-0.01072969  0.00865408 -0.02301539]]
-# b1 = [[0.]
-#  [0.]]
-# W2 = [[ 0.01744812 -0.00761207]]
-# b2 = [[0.]]
-# ```
-
-# <a name='3-2'></a>
-# ### 3.2 - L-layer Neural Network
-# 
-# The initialization for a deeper L-layer neural network is more complicated because there are many more weight matrices and bias vectors. When completing the `initialize_parameters_deep` function, you should make sure that your dimensions match between each layer. Recall that $n^{[l]}$ is the number of units in layer $l$. For example, if the size of your input $X$ is $(12288, 209)$ (with $m=209$ examples) then:
-# 
-# <table style="width:100%">
-#     <tr>
-#         <td>  </td> 
-#         <td> <b>Shape of W</b> </td> 
-#         <td> <b>Shape of b</b>  </td> 
-#         <td> <b>Activation</b> </td>
-#         <td> <b>Shape of Activation</b> </td> 
-#     <tr>
-#     <tr>
-#         <td> <b>Layer 1</b> </td> 
-#         <td> $(n^{[1]},12288)$ </td> 
-#         <td> $(n^{[1]},1)$ </td> 
-#         <td> $Z^{[1]} = W^{[1]}  X + b^{[1]} $ </td> 
-#         <td> $(n^{[1]},209)$ </td> 
-#     <tr>
-#     <tr>
-#         <td> <b>Layer 2</b> </td> 
-#         <td> $(n^{[2]}, n^{[1]})$  </td> 
-#         <td> $(n^{[2]},1)$ </td> 
-#         <td>$Z^{[2]} = W^{[2]} A^{[1]} + b^{[2]}$ </td> 
-#         <td> $(n^{[2]}, 209)$ </td> 
-#     <tr>
-#        <tr>
-#         <td> $\vdots$ </td> 
-#         <td> $\vdots$  </td> 
-#         <td> $\vdots$  </td> 
-#         <td> $\vdots$</td> 
-#         <td> $\vdots$  </td> 
-#     <tr>  
-#    <tr>
-#        <td> <b>Layer L-1</b> </td> 
-#         <td> $(n^{[L-1]}, n^{[L-2]})$ </td> 
-#         <td> $(n^{[L-1]}, 1)$  </td> 
-#         <td>$Z^{[L-1]} =  W^{[L-1]} A^{[L-2]} + b^{[L-1]}$ </td> 
-#         <td> $(n^{[L-1]}, 209)$ </td> 
-#    <tr>
-#    <tr>
-#        <td> <b>Layer L</b> </td> 
-#         <td> $(n^{[L]}, n^{[L-1]})$ </td> 
-#         <td> $(n^{[L]}, 1)$ </td>
-#         <td> $Z^{[L]} =  W^{[L]} A^{[L-1]} + b^{[L]}$</td>
-#         <td> $(n^{[L]}, 209)$  </td> 
-#     <tr>
-# </table>
-# 
-# Remember that when you compute $W X + b$ in python, it carries out broadcasting. For example, if: 
-# 
-# $$ W = \begin{bmatrix}
-#     w_{00}  & w_{01} & w_{02} \\
-#     w_{10}  & w_{11} & w_{12} \\
-#     w_{20}  & w_{21} & w_{22} 
-# \end{bmatrix}\;\;\; X = \begin{bmatrix}
-#     x_{00}  & x_{01} & x_{02} \\
-#     x_{10}  & x_{11} & x_{12} \\
-#     x_{20}  & x_{21} & x_{22} 
-# \end{bmatrix} \;\;\; b =\begin{bmatrix}
-#     b_0  \\
-#     b_1  \\
-#     b_2
-# \end{bmatrix}\tag{2}$$
-# 
-# Then $WX + b$ will be:
-# 
-# $$ WX + b = \begin{bmatrix}
-#     (w_{00}x_{00} + w_{01}x_{10} + w_{02}x_{20}) + b_0 & (w_{00}x_{01} + w_{01}x_{11} + w_{02}x_{21}) + b_0 & \cdots \\
-#     (w_{10}x_{00} + w_{11}x_{10} + w_{12}x_{20}) + b_1 & (w_{10}x_{01} + w_{11}x_{11} + w_{12}x_{21}) + b_1 & \cdots \\
-#     (w_{20}x_{00} + w_{21}x_{10} + w_{22}x_{20}) + b_2 &  (w_{20}x_{01} + w_{21}x_{11} + w_{22}x_{21}) + b_2 & \cdots
-# \end{bmatrix}\tag{3}  $$
-# 
-
-# <a name='ex-2'></a>
-# ### Exercise 2 -  initialize_parameters_deep
-# 
-# Implement initialization for an L-layer Neural Network. 
-# 
-# **Instructions**:
-# - The model's structure is *[LINEAR -> RELU] $ \times$ (L-1) -> LINEAR -> SIGMOID*. I.e., it has $L-1$ layers using a ReLU activation function followed by an output layer with a sigmoid activation function.
-# - Use random initialization for the weight matrices. Use `np.random.randn(shape) * 0.01`.
-# - Use zeros initialization for the biases. Use `np.zeros(shape)`.
-# - You'll store $n^{[l]}$, the number of units in different layers, in a variable `layer_dims`. For example, the `layer_dims` for last week's Planar Data classification model would have been [2,4,1]: There were two inputs, one hidden layer with 4 hidden units, and an output layer with 1 output unit. This means `W1`'s shape was (4,2), `b1` was (4,1), `W2` was (1,4) and `b2` was (1,1). Now you will generalize this to $L$ layers! 
-# - Here is the implementation for $L=1$ (one layer neural network). It should inspire you to implement the general case (L-layer neural network).
-# ```python
-#     if L == 1:
-#         parameters["W" + str(L)] = np.random.randn(layer_dims[1], layer_dims[0]) * 0.01
-#         parameters["b" + str(L)] = np.zeros((layer_dims[1], 1))
-# ```
-
-# In[5]:
-
 
 # GRADED FUNCTION: initialize_parameters_deep
 
@@ -741,9 +514,6 @@ def update_parameters(params, grads, learning_rate):
     return parameters
 
 
-# In[27]:
-
-
 t_parameters, grads = update_parameters_test_case()
 t_parameters = update_parameters(t_parameters, grads, 0.1)
 
@@ -755,32 +525,4 @@ print ("b2 = "+ str(t_parameters["b2"]))
 update_parameters_test(update_parameters)
 
 
-# **Expected output:**
-# 
-# ```
-# W1 = [[-0.59562069 -0.09991781 -2.14584584  1.82662008]
-#  [-1.76569676 -0.80627147  0.51115557 -1.18258802]
-#  [-1.0535704  -0.86128581  0.68284052  2.20374577]]
-# b1 = [[-0.04659241]
-#  [-1.28888275]
-#  [ 0.53405496]]
-# W2 = [[-0.55569196  0.0354055   1.32964895]]
-# b2 = [[-0.84610769]]
-# ```
-
 # ### Congratulations! 
-# 
-# You've just implemented all the functions required for building a deep neural network, including: 
-# 
-# - Using non-linear units improve your model
-# - Building a deeper neural network (with more than 1 hidden layer)
-# - Implementing an easy-to-use neural network class
-# 
-# This was indeed a long assignment, but the next part of the assignment is easier. ;) 
-# 
-# In the next assignment, you'll be putting all these together to build two models:
-# 
-# - A two-layer neural network
-# - An L-layer neural network
-# 
-# You will in fact use these models to classify cat vs non-cat images! (Meow!) Great work and see you next time. 
